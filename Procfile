c4448032d9d071e9eb30f3754fb532c9a40e27c9
Jacek Wyrostek <jacek.wyrostek@gmail.com>
2021-02-27 10:30:49 +0100

    heroku set up

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Quotes.wsgi --log-file - 
--- Quotes/settings.py
+++ Quotes/settings.py
@@ -11,19 +11,17 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
+SECRET_KEY = config('SECRET_KEY')
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pl80i0u2acjkagz*uj*44aqqn#wm*$^s$hjotwodtz$g$cjxg='
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -75,12 +73,8 @@ WSGI_APPLICATION = 'Quotes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+deafault_dburl='sqlite:///'+os.path.join(BASE_DIR, 'db.sqlite3')
+DATABASES={'default':config('DATABASE_URL', default=deafault_dburl, cast=dburl)}
 
 
 # Password validation
@@ -120,6 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = ['static']
 
 LOGIN_URL = 'login'
