614507e10ece887cf6e9a73cfa55557967bef932
Jacek Wyrostek <jacek.wyrostek@gmail.com>
2021-02-27 19:41:16 +0100

    os error fixing

--- Quotes/settings.py
+++ Quotes/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from os import *
+import os
 from pathlib import Path
 from decouple import config
 from dj_database_url import parse as dburl
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'Quotes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-deafault_dburl='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+default_dburl='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 DATABASES={'default':config('DATABASE_URL', default=deafault_dburl, cast=dburl)}
 
 
